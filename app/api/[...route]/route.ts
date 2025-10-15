5e85dec2e66c3742a369947e82c6bb789b402f1b
imad <113237261+medjdene22@users.noreply.github.com>
2025-10-15 02:17:12 +0100

    2

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -34,3 +34,6 @@ app
     return c.text(welcomeStrings.join("\n\n"));
   });
 export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
