767cb94485c5a61113ec17496cec090fe86ca1ff
imad <113237261+medjdene22@users.noreply.github.com>
2025-10-15 15:26:45 +0100

    9

--- app/api/[...route]/posts.ts
+++ app/api/[...route]/posts.ts
@@ -1,39 +1,66 @@
 import { Hono } from "hono";
 import { auth } from "@/lib/auth/auth";
 import { db } from "@/db/index";
-import { favorite } from "@/db/schema";
+import { favorite, insertFavSchima } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { string, z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono()
-  .get("/seed", async (c) => {
-    const res = await auth.api.signInEmail({
-      headers: c.req.raw.headers,
-
-      body: {
-        email: "imad55777@gmail.com",
-        // name: "imad eddine",
-        password: "imad55777",
-      },
-    });
-
-    return c.json({ res: res });
-  })
-  .get("/", async (c) => {
+  .get("/favorite", async (c) => {
     const session = await auth.api.getSession({
       headers: c.req.raw.headers,
     });
-
     if (!session?.user) {
       return c.json({ error: "Unauthorized" }, 403);
     }
 
-    // const favorites = await db
-    //   .select({
-    //     id: favorite.id,
+    const favorites = await db
+      .select({
+        id: favorite.id,
 
-  // return c.json({ favorites });
-  //
-  return c.json({ session });
-});
+        postId: favorite.postId,
+      })
+      .from(favorite)
+      .where(eq(favorite.userId, session.user.id));
+
+    return c.json({ favorites });
+  })
+  .post(
+    "/",
+    zValidator(
+      "json",
+      insertFavSchima.pick({
+        postId: true,
+      }),
+    ),
+    async (c) => {
+      const session = await auth.api.getSession({
+        headers: c.req.raw.headers,
+      });
+
+      if (!session?.user) {
+        return c.json({ error: "Unauthorized" }, 403);
+      }
+
+      const values = c.req.valid("json");
+
+      await db
+        .insert(favorite)
+        .values({
+          userId: session.user.id,
+          postId: values.postId,
+        })
+        .onConflictDoNothing();
+
+      // const favorites = await db
+      //   .select({
+      //     id: favorite.id,
+
+      // return c.json({ favorites });
+      //
+      return c.json({ session });
+    },
+  );
 
 export default app;
