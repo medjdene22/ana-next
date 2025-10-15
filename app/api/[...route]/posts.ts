855be2da7fbc2a5f814211698538f86d750b201e
imad <113237261+medjdene22@users.noreply.github.com>
2025-10-15 10:23:43 +0100

    6

--- app/api/[...route]/posts.ts
+++ app/api/[...route]/posts.ts
@@ -1,26 +1,35 @@
 import { Hono } from "hono";
 import { auth } from "@/lib/auth/auth";
-import { db } from "@/db/index.js";
+import { db } from "@/db/index";
 import { favorite } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
-const app = new Hono().get("/", async (c) => {
-  const session = await auth.api.getSession({
-    headers: c.req.raw.headers,
-  });
+const app = new Hono()
+  .get("/seed", async (c) => {
+    const res = await auth.api.signInEmail({
+      headers: c.req.raw.headers,
 
-  if (!session?.user) {
-    return c.json({ error: "Unauthorized" }, 403);
-  }
+      body: {
+        email: "imad55777@gmail.com",
+        // name: "imad eddine",
+        password: "imad55777",
+      },
+    });
 
-  // const favorites = await db
-  //   .select({
-  //     id: favorite.id,
+    return c.json({ res: res });
+  })
+  .get("/", async (c) => {
+    const session = await auth.api.getSession({
+      headers: c.req.raw.headers,
+    });
 
-  //     postId: favorite.postId,
-  //   })
-  //   .from(favorite)
-  //   .where(eq(favorite.userId, session.user.id));
+    if (!session?.user) {
+      return c.json({ error: "Unauthorized" }, 403);
+    }
+
+    // const favorites = await db
+    //   .select({
+    //     id: favorite.id,
 
   // return c.json({ favorites });
   //
